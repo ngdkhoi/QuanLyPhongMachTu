2b3df63d5983d3d1f5cc4c2daf57954bb01a31ed
ngdkhoi <49549370+ngdkhoi@users.noreply.github.com>
2021-06-08 10:50:33 +0700

    Update Patients' List Screen

---
+++ QuanLyPhongMachTu/QuanLyPhongMachTu/Converter/DateConveter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Globalization;
+
+namespace QuanLyPhongMachTu.dateConverter
+{
+    public class DateConveter :IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string strD = value.ToString();
+            string[] DateAndTime = strD.Split(' ');
+            string[] tmp = DateAndTime[0].Split('/');
+            if (tmp[0].Length == 1)
+            {
+                tmp[0] = "0" + tmp[0];
+            }
+            if (tmp[1].Length == 1)
+            {
+                tmp[1] = "0" + tmp[1];
+            }
+            string Date = "";
+            Date = tmp[1] + "/" + tmp[0] + "/" + tmp[2];
+            Date = Date.TrimEnd('/');
+            return Date;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ QuanLyPhongMachTu/QuanLyPhongMachTu/ViewModel/PatientListViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace QuanLyPhongMachTu.ViewModel
+{
+    public class PatientListViewModel : BaseViewModel
+    {
+        //private ObservableCollection<Patient> _Patients = new ObservableCollection<Patient>();
+        private DateTime _Date;
+        public DateTime Date { get => _Date;  set { _Date = value; OnPropertyChanged(); } }
+
+        //private ObservableCollection<Patient> Patients 
+        //{ 
+        //    get => _Patients; 
+        //    set 
+        //    { 
+        //        _Patients = value;
+        //        OnPropertyChanged();
+        //    } 
+        //}
+
+        public PatientListViewModel()
+        {
+            Date = DateTime.Now;
+           // Patient temp = new Patient() { stt = 1, Name = "Nguyễn Đình Khôi", Gender = "Nam", YoB = 2000, Address = "Gò Vấp" };
+            
+        }
+    }
+}
