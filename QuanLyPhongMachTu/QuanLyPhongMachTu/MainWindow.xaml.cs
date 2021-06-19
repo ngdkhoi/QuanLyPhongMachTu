9f3c251b2c0a83b2167b69af50dd2fdd6e8b411c
mrduyfit18 <18120171@student.hcmus.edu.vn>
2021-06-19 16:52:30 +0700

    Create Diagnosis Screen

--- QuanLyPhongMachTu/QuanLyPhongMachTu/MainWindow.xaml.cs
+++ QuanLyPhongMachTu/QuanLyPhongMachTu/MainWindow.xaml.cs
@@ -23,6 +23,7 @@ namespace QuanLyPhongMachTu
         public MainWindow()
         {
             InitializeComponent();
+            ListViewMenu.SelectedIndex = 1;
         }
 
         private void ListViewMenu_SelectionChanged(object sender, SelectionChangedEventArgs e)
