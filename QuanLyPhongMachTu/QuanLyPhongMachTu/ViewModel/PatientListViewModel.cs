c663263ee968cde139b0813681913e1603908e5c
ngdkhoi <49549370+ngdkhoi@users.noreply.github.com>
2021-06-22 20:33:18 +0700

    update Pay screen

--- QuanLyPhongMachTu/QuanLyPhongMachTu/ViewModel/PatientListViewModel.cs
+++ QuanLyPhongMachTu/QuanLyPhongMachTu/ViewModel/PatientListViewModel.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
+using System.IO;
 
 namespace QuanLyPhongMachTu.ViewModel
 {
@@ -12,6 +14,7 @@ namespace QuanLyPhongMachTu.ViewModel
         //private ObservableCollection<Patient> _Patients = new ObservableCollection<Patient>();
         private DateTime _Date;
         public DateTime Date { get => _Date;  set { _Date = value; OnPropertyChanged(); } }
+        public ICommand PayCommand { get; set; }
 
         //private ObservableCollection<Patient> Patients 
         //{ 
@@ -22,12 +25,19 @@ namespace QuanLyPhongMachTu.ViewModel
         //        OnPropertyChanged();
         //    } 
         //}
-
+        
         public PatientListViewModel()
         {
             Date = DateTime.Now;
-           // Patient temp = new Patient() { stt = 1, Name = "Nguyễn Đình Khôi", Gender = "Nam", YoB = 2000, Address = "Gò Vấp" };
-            
+            // Patient temp = new Patient() { stt = 1, Name = "Nguyễn Đình Khôi", Gender = "Nam", YoB = 2000, Address = "Gò Vấp" };
+            PayCommand = new RelayCommand<PatientListScreen>((p) =>
+              {
+                  return true;
+              }, (p) =>
+              {
+                  PayScreen ps = new PayScreen();
+                  ps.Show();
+              });
         }
     }
 }
