2dffb834fd4e2c9340f7c5d2184b5e254ba8927e
ngdkhoi <49549370+ngdkhoi@users.noreply.github.com>
2021-06-28 17:48:32 +0700

    currency converter

---
+++ QuanLyPhongMachTu/QuanLyPhongMachTu/Converter/CurrencyConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Globalization;
+
+namespace QuanLyPhongMachTu.Converter
+{
+    public class VNDConvert : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var temp = Int32.Parse(value.ToString());
+            if (temp == 0)
+            {
+                return temp;
+            }
+            string con = temp.ToString("###,###,###,### VND").Replace(',', '.');
+            return con;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
