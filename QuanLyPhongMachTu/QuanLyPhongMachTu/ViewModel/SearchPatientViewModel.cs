ff427a17a45a8f0405c265cafdceec8baf3bdf25
ngdkhoi <49549370+ngdkhoi@users.noreply.github.com>
2021-06-29 08:00:49 +0700

    search screen

---
+++ QuanLyPhongMachTu/QuanLyPhongMachTu/ViewModel/SearchPatientViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuanLyPhongMachTu.Model;
+
+namespace QuanLyPhongMachTu.ViewModel
+{
+    public class SearchPatientViewModel : BaseViewModel
+    {
+        private DateTime _Date;
+        public SearchPatientViewModel() { }
+
+        public DateTime Date { get => _Date; set => _Date = value; }
+    }
+}
