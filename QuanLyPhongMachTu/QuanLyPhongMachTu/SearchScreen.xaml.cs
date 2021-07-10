98783a3354e3e4872b811556d486cceb4e6aa4eb
ngdkhoi <49549370+ngdkhoi@users.noreply.github.com>
2021-07-10 20:16:14 +0700

    update search patients function

--- QuanLyPhongMachTu/QuanLyPhongMachTu/SearchScreen.xaml.cs
+++ QuanLyPhongMachTu/QuanLyPhongMachTu/SearchScreen.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using QuanLyPhongMachTu.ViewModel;
 
 namespace QuanLyPhongMachTu
 {
@@ -20,9 +21,11 @@ namespace QuanLyPhongMachTu
     /// </summary>
     public partial class SearchScreen : UserControl
     {
+        public SearchPatientViewModel VM { get; set; }
         public SearchScreen()
         {
             InitializeComponent();
+            this.DataContext = VM = new SearchPatientViewModel();
         }
     }
 }
--- QuanLyPhongMachTu/QuanLyPhongMachTu/ViewModel/SearchPatientViewModel.cs
+++ QuanLyPhongMachTu/QuanLyPhongMachTu/ViewModel/SearchPatientViewModel.cs
@@ -67,6 +67,7 @@ namespace QuanLyPhongMachTu.ViewModel
         }
         public SearchPatientViewModel() 
         {
+            LoadData();
         }
 
         
